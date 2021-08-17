35515f487868ea00bf7abfb05ee26a16bf07c473
Jonathan Small <JSmall@cxc.org>
2021-08-17 11:39:04 -0400

    Removed unused code from class-connection.php

--- includes/class-connection.php
+++ includes/class-connection.php
@@ -4,36 +4,35 @@
 
     private $username = get_option( 'magento_admin_username' );
     private $password = get_option( 'magento_admin_password' );
-    private $adminToken;
 
     class MGConnection {
-        protected function getMGAdminToken (){
+
+        protected function createMGAdminToken (){
+
             $curl = curl_init();
-
-            curl_setopt($curl, CURLOPT_POST, 1);
-        
             curl_setopt($curl, CURLOPT_URL, $this->$url);
-
-            curl_setopt($curl, CURLOPT_USERPWD, $username . ":" . $password);
-
-            curl_setopt($curl, CURLOPT_HTTPHEADER, array(
-            'Content-Type: application/json',
-            ));
-
+            curl_setopt($curl, CURLOPT_POST, 1);
             curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
 
+            $headers = array(
+                "Content-Type: application/json",
+            );
+            curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
+
+            $data_array =  array(
+                "username"     => $this->$username,
+                "password"     => $this->$password,
+            );
+            curl_setopt($curl, CURLOPT_POSTFIELDS, $data_array);
+            
             $result = curl_exec( $curl );
             
             if( !$result ){
                 die("Connection Failure");
-            } else{
-                $adminToken = $result;
-            }
+            } 
 
             curl_close($curl);
-    }
 
-    protected function getToken(){
-        return $adminToken;
+            return $result;
     }
 ?>
